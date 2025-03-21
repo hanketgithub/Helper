2cd6765f3f1a3c0b8e5cd779916435b26df1bc1f
hanketgithub <hanketgoog@gmail.com>
2025-03-21 17:53:14 +0800

    First check in

---
+++ Makefile
@@ -0,0 +1,17 @@
+target  = helper
+sources = main.c $(target).c
+objects = $(patsubst %.c,%.o,$(sources))
+CC = gcc
+OPTS = -Wall -O2
+
+all: $(objects)
+	$(CC) $(OPTS) -o $(target) $(objects)
+
+$(target).o: $(target).c
+	$(CC) $(OPTS) -c $<
+
+install:
+	cp $(target) /usr/local/bin
+
+clean:
+	$(RM) $(target) $(objects)
---
+++ helper.cpp
@@ -0,0 +1,31 @@
+static void print_binary(const char *label, uint8_t *data, uint32_t size)
+{
+  const int bytes_per_line = 16;
+
+  printf("%s, size = %u\n", label, size);
+
+  for (uint32_t offset = 0; offset < size; offset += bytes_per_line) {
+    printf("%08x  ", offset);
+
+    int cur_line_bytes = (size - offset < bytes_per_line) ? (size - offset) : bytes_per_line;
+
+    for (int i = 0; i < cur_line_bytes; ++i) {
+      printf("%02x ", data[offset + i]);
+      if (i == 7) printf(" ");  // extran space after 8 byte
+    }
+
+    // add space to align
+    for (int i = cur_line_bytes; i < bytes_per_line; ++i) {
+      printf("   ");
+      if (i == 7) printf(" ");
+    }
+
+    // ASCII section
+    printf(" |");
+    for (int i = 0; i < cur_line_bytes; ++i) {
+      unsigned char c = data[offset + i];
+      printf("%c", (c >= 32 && c <= 126) ? c : '.');
+    }
+    printf("|\n");
+  }
+}
